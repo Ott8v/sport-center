4b07e1a269ab583f2e459495a7c19a58f959edf4
Federico Balducci <ott8vv@gmail.com>
2025-06-08 17:04:33 +0200

    ✨ Add logic to Auth controller

--- app/controllers/auth_controller.ts
+++ app/controllers/auth_controller.ts
@@ -1,17 +1,32 @@
-// import type { HttpContext } from '@adonisjs/core/http'
+import type { HttpContext } from '@adonisjs/core/http'
+
+import User from '#models/user'
 
 export default class AuthController {
-  async login() {
-    // Logic for user login
-    return {
-      message: 'User logged in successfully',
+  async login({ request, response }: HttpContext) {
+    const email = request.input('email')
+    const password = request.input('password')
+    const user = await User.verifyCredentials(email, password)
+    if (!user) {
+      return response.unauthorized('Invalid credentials')
     }
+    const token = await User.accessTokens.create(user)
+    return { user, token }
   }
 
-  async register() {
-    // Logic for user registration
-    return {
-      message: 'User registered successfully',
+  async register({ request, response }: HttpContext) {
+    const { email, password, name, surname } = request.all()
+    try {
+      const user = await User.create({
+        email,
+        password,
+        name,
+        surname,
+      })
+      const token = await User.accessTokens.create(user)
+      return { user, token }
+    } catch (error) {
+      return response.badRequest('Registration failed')
     }
   }
 }
--- app/models/user.ts
+++ app/models/user.ts
@@ -15,13 +15,19 @@ export default class User extends compose(BaseModel, AuthFinder) {
   declare id: number
 
   @column()
-  declare fullName: string | null
+  declare name: string
+
+  @column()
+  declare surname: string
 
   @column()
   declare email: string
 
   @column({ serializeAs: null })
   declare password: string
+
+  @column()
+  declare role: 'admin' | 'staff' | 'user'
 
   @column.dateTime({ autoCreate: true })
   declare createdAt: DateTime
