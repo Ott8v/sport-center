9ceed1f850f10aa624fe13a6b712d2cc7734ed91
Federico Balducci <ott8vv@gmail.com>
2025-06-11 17:47:02 +0200

    ✨ Add user factory

---
+++ database/factories/user_factory.ts
@@ -0,0 +1,13 @@
+import factory from '@adonisjs/lucid/factories'
+import User from '#models/user'
+
+export const UserFactory = factory
+  .define(User, async ({ faker }) => {
+    return {
+      name: faker.person.firstName(),
+      surname: faker.person.lastName(),
+      email: faker.internet.email(),
+      password: faker.internet.password(),
+    }
+  })
+  .build()
