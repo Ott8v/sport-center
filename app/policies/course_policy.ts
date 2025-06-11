a667509ce936bfb87a26402aaa18e8fab03db480
Federico Balducci <ott8vv@gmail.com>
2025-06-11 17:47:23 +0200

    ✨ Add course policy

---
+++ app/policies/course_policy.ts
@@ -0,0 +1,14 @@
+import User from '#models/user'
+import Course from '#models/course'
+import { BasePolicy } from '@adonisjs/bouncer'
+import { AuthorizerResponse } from '@adonisjs/bouncer/types'
+
+export default class CoursePolicy extends BasePolicy {
+  update(user: User, course: Course): AuthorizerResponse {
+    return user.id === course.intructorId || user.role === 'admin'
+  }
+
+  delete(user: User, course: Course): AuthorizerResponse {
+    return user.id === course.intructorId || user.role === 'admin'
+  }
+}
